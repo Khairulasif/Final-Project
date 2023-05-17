1b814dd6b2c58090b00a00aca5d6556ac3821e9c
Md. Khairul Islam <104551563+Khairulasif@users.noreply.github.com>
2023-05-17 15:09:14 +0600

    class change

--- src/app/add-admin/add-admin.component.ts
+++ src/app/add-admin/add-admin.component.ts
@@ -1,10 +1,79 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { modelType } from './model';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ServiceService } from './service.service';
 
 @Component({
   selector: 'app-add-admin',
   templateUrl: './add-admin.component.html',
   styleUrls: ['./add-admin.component.css']
 })
-export class AddAdminComponent {
+export class AddAdminComponent implements OnInit{
+  
+
+  postList!: modelType[];
+  form2 !: FormGroup;
+  // fromDate: any; toDate: any;
+  constructor(public services: ServiceService, private router: Router) { }
+
+
+  ngOnInit(): void {
+
+    this.services.getTask().subscribe((newPost: modelType[]) => {
+      this.postList = newPost; console.log(this.postList)
+    })
+
+    this.form2 = new FormGroup({
+      id: new FormControl(),
+      name: new FormControl(),
+      email: new FormControl(),
+      userName: new FormControl(),
+      password: new FormControl(),
+      retypePassword: new FormControl(),
+
+
+    })
+
+    this.editData=false;
+  }
+
+  editData: boolean = false;
+
+
+  edit(task:modelType) {
+    this.editData=true;
+    this.form2 = new FormGroup({
+      id: new FormControl(task.id),
+      name: new FormControl(task.name),
+      email: new FormControl(task.email),
+      
+      userName: new FormControl(task.userName),
+      password: new FormControl(task.password),
+      retypePassword: new FormControl(task.retypePassword),
+      
+    })
+  }
+
+  submit() {
+    console.log(this.form2.value);
+    this.services.addTask(this.form2.value).subscribe((res: any) => {
+      console.log('Post created successfully');
+      this.ngOnInit();
+      // this.router.navigateByUrl('holidayList');
+    })
+
+  }
+
+  deleteTask(id: number) {
+
+
+    this.services.deleteTask(id).subscribe(res => {
+       this.postList = this.postList.filter(item => item.id !== id);
+      console.log('Post deleted successfully!');
+      //  this.services.getTask().subscribe((newPost: modelType[]) => {this.postList = newPost; console.log(this.postList)})
+    })
+
+  }
 
 }
--- src/app/add-admin/service.service.ts
+++ src/app/add-admin/service.service.ts
@@ -1,9 +1,40 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { modelType } from './model';
 
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+}
 @Injectable({
   providedIn: 'root'
 })
 export class ServiceService {
 
-  constructor() { }
+  private url = 'http://localhost:8080/api';
+
+
+  constructor(private httpService: HttpClient) {}
+
+  getTask() : Observable<modelType[]> {
+    const task = this.httpService.get<modelType[]>(this.url + '/adminlistget');
+    return task;
+  }
+
+  addTask(task:modelType) {
+    return this.httpService.post<modelType>(this.url + '/addadminpost' , task, httpOptions);
+  }
+
+  updateTask(task:modelType) {
+
+    return this.httpService.put<modelType>(this.url + '/posts/' + task.id, task, httpOptions)
+  }
+
+  deleteTask(id:number) {
+
+    return this.httpService.delete(this.url + '/holidaylistdelete/'+ id)
+  }
 }
